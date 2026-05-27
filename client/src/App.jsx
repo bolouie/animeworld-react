9501f54396d998155702f901f2c3ac6837c227cf
Bo Louie <bls@Bos-MacBook-Pro.local>
2026-05-26 23:06:46 -0400

    Initial template: Vite + React + Tailwind client, Flask server

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-950 text-white">
+      <h1 className="text-3xl font-bold text-purple-400 p-8">
+        AnimeWorld
+      </h1>
+    </div>
+  )
+}
+
+export default App
