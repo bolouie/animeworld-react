af3412058e2620ac295b440e776e6d2cb5635874
Bo Louie <bo.louie.dev@gmail.com>
2026-06-06 18:30:42 -0400

    feat: add isWizardOpen and selectedPackage state to App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
+import { useState } from 'react'
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
 import Packages from "./components/Packages.jsx";
 
 function App() {
+  const [isWizardOpen, setIsWizardOpen] = useState(false)
+  const [selectedPackage, setSelectedPackage] = useState(null)
   return (
     <>
       <Navbar />
