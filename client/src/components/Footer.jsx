f743591e0014ad480a2ede434c79a1936a1029aa
Bo Louie <bo.louie.dev@gmail.com>
2026-06-15 21:05:20 -0400

    style: semantic cleanup on Footer, swap p for small, remove fragment

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,17 +2,12 @@ import { FaGithub } from 'react-icons/fa'
 function Footer() {
     return (
         <footer className="grid grid-cols-1 sm:grid-cols-[1fr_2fr_1fr] gap-4 px-8 py-6 text-center">
-            <>
-                <a href="#home" aria-label="AnimeWorld home" className="text-white font-bold text-xl">Anime<span className="text-anime-orange ">World</span></a>
-                <p>Copyright © 2026 React+TailwindCSS+Flask+PostgreSQL </p>
-                <a href="https://github.com/bolouie" aria-label="Github Profile" className="flex items-center justify-center">
-
-                    <FaGithub className="w-6 h-6 text-white" />
-                </a>
-
-            </>
+            <a href="#home" aria-label="AnimeWorld home" className="text-white font-bold text-xl">Anime<span className="text-anime-orange ">World</span></a>
+            <small>Copyright © 2026 React+TailwindCSS+Flask+PostgreSQL</small>
+            <a href="https://github.com/bolouie" aria-label="Github Profile" className="flex items-center justify-center">
+                <FaGithub className="w-6 h-6 text-white" />
+            </a>
         </footer>
-
     )
 }
 
